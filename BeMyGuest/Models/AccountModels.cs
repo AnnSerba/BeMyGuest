b05a14df1f60b8ba56d57f839a1a2c199b676261
Аня Серба <annserba94@gmail.com>
2015-05-04 23:44:31 +0300

    Commit3

--- BeMyGuest/Models/AccountModels.cs
+++ BeMyGuest/Models/AccountModels.cs
@@ -11,7 +11,7 @@ namespace BeMyGuest.Models
     public class UsersContext : DbContext
     {
         public UsersContext()
-            : base("DefaultConnection")
+            : base("aspnet-BeMyGuest-20150425021944")
         {
         }
 
